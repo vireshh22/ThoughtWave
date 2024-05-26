aef61b163a08534b79c3a184d749c5aa3a591d9e
vireshh22 <viresh0489@gmail.com>
2024-05-26 08:47:37 +0530

    Added Helper Package

--- src/main/java/com/entities/Note.java
+++ src/main/java/com/entities/Note.java
@@ -1,6 +1,60 @@
 package com.entities;
 
+import java.util.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "notes")
 public class Note {
+    @Id
     private int id;
     private String title;
+    private String content;
+    private Date addedDate;
+
+    public Note() {
+    }
+
+    public Note(String title, String content, Date addedDate) {
+        this.id = new Random().nextInt(100000);
+        this.title = title;
+        this.content = content;
+        this.addedDate = addedDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAddedDate(Date addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getAddedDate() {
+        return addedDate;
+    }
+
 }
